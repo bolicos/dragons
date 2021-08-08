268376e516b917d1103b8482ce7783e2ef55309e
analuciabolico <analuciabolico.dev@gmail.com>
2021-08-07 23:19:54 -0300

    FEAT -- Update Title Component

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { TitleProps } from "@/models/attributes-props";
 import stylesheet from "./stylesheet.module.scss";
 
-export const Title: React.FC<TitleProps> = ({ name, ...props }) => {
-  return <div className={stylesheet["title"]}>TITLE</div>;
+export const Title: React.FC<TitleProps> = ({
+  name,
+  ...props
+}) => {
+  return <h1 className={stylesheet["title"]}>{name}</h1>;
 };
 
 export default Title;
