b2cedf98f81b6a17a09d41b428df15620b34e2fa
analuciabolico <analuciabolico.dev@gmail.com>
2021-08-08 00:16:43 -0300

    FEAT -- Update Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
+import clsx from "clsx";
 import Loader from "@/components/Loader";
 import Title from "@/components/Title";
-import Container from "@/components/Container";
 import Image from "@/components/Image";
-import WallpaperImage from "@/assets/images/kali.png";
-import { ReactComponent as LogoImage } from "@/assets/images/logo.svg";
 import { DefaultState } from "@/models/default";
+import LogoImage from "@/assets/images/logo.jpg";
 import stylesheet from "./stylesheet.module.scss";
+import Button from "#/src/components/Button";
 
 interface State extends DefaultState {
   title: string;
@@ -28,11 +28,15 @@ export const Home: React.FC = () => {
   return state.pending === true ? (
     <Loader />
   ) : (
-    <Container>
-      <LogoImage />
-      <Title name={state.title} className={stylesheet[""]} />
-      <Image image={WallpaperImage} alt="wallpaper" />
-    </Container>
+    <>
+        <Title name={state.title} />
+        <Image
+          image={LogoImage}
+          alt="logo"
+          className={clsx(stylesheet["logo"])}
+        />
+      <Button>SignIn</Button>
+    </>
   );
 };
 
