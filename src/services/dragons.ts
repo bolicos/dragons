ea8cbf9e2e33267c443ac8aa0f054de0fdf7efb8
analuciabolico <analuciabolico.dev@gmail.com>
2021-08-09 02:16:17 -0300

    FEAT -- Add routes and new api call into service

--- src/services/dragons.ts
+++ src/services/dragons.ts
@@ -6,16 +6,16 @@ export const ENDPOINTS = {
   CREATE: () => "/api/v1/dragon",
   FIND_BY_ID: (id: string) => `/api/v1/dragon/${id}`,
   DELETE_BY_ID: (id: string) => `/api/v1/dragon/${id}`,
-  UPDATE_BY_ID: (id: string) => `/api/v1/dragon/${id}`,
+  EDIT_BY_ID: (id: string) => `/api/v1/dragon/${id}`,
 };
 
-interface List<T> extends Promise<Response<Array<T>>> {}
-interface Object<T> extends Promise<Response<T>> {}
+interface List<T> extends Promise<Response<Array<T>>> { }
+interface Object<T> extends Promise<Response<T>> { }
 
 export const DragonsAPI = {
   list: (): List<DragonModel> => client.get<Array<DragonModel>>(ENDPOINTS.LIST()),
   delete: (id: string): Object<void> => client.delete(ENDPOINTS.DELETE_BY_ID(id)),
   details: (id: string): Object<DragonModel> => client.get(ENDPOINTS.FIND_BY_ID(id)),
   create: (body: DragonModel): Object<DragonModel> => client.post(ENDPOINTS.CREATE(), body),
-  update: (body: DragonModel, id: string): Object<DragonModel> => client.put(ENDPOINTS.UPDATE_BY_ID(id), body),
+  edit: (id: string, body: DragonModel,): Object<DragonModel> => client.put(ENDPOINTS.EDIT_BY_ID(id), body),
 };
