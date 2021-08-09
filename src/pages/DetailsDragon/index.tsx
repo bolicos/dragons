fc9db99a7f1934f31efad77ace304d003fd8b3e7
analuciabolico <analuciabolico.dev@gmail.com>
2021-08-08 22:48:23 -0300

    FEAT -- Add new page Details of the Dragon

---
+++ src/pages/DetailsDragon/index.tsx
@@ -0,0 +1,56 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Loader from "@/components/Loader";
+import Title from "@/components/Title";
+import { DefaultState } from "@/models/default";
+import { DragonsAPI } from "@/services/dragons";
+import { DragonModel } from "@/models/dragons";
+import { GenericModel } from "@/models/props";
+import Details from "@/components/Details";
+
+interface State extends DefaultState {
+  title: string;
+  dragon: DragonModel;
+}
+
+export const DetailsDragon: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const [state, setState] = useState<State>({
+    pending: true,
+    title: "Details",
+    dragon: {} as DragonModel,
+  });
+
+  const details: GenericModel[] = [
+    { title: "Name", function: "name" },
+    { title: "Type", function: "type" },
+    { title: "Created At", function: "createdAt" },
+    { title: "Histories", function: "histories" },
+  ];
+
+
+  const detailsDragon = useCallback(() => {
+    DragonsAPI.details(id)
+      .then((response) => {
+        const dragon: DragonModel = response.data;
+        setState((old) => ({ ...old, dragon: dragon }));
+      })
+      .catch((exception) => console.log("API error: ", exception))
+      .finally(() => setState((old) => ({ ...old, pending: false })));
+  }, [id]);
+
+  useEffect(() => {
+    detailsDragon();
+  }, [detailsDragon]);
+
+  return state.pending === true ? (
+    <Loader />
+  ) : (
+    <>
+      <Title name={state.title} />
+      <Details content={state.dragon} details={details} />
+    </>
+  );
+};
+
+export default DetailsDragon;
